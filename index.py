4c7e990f7fd9f8e44d736d735ee4f645fc35be3f
Kamil-shehzad <75947637+Kamil-shehzad@users.noreply.github.com>
2024-10-18 19:54:13 -0700

    Print some in python(First python program

---
+++ index.py
@@ -0,0 +1,18 @@
+print('Hello World')
+print('world are moving to Ai')
+print('Hello my name is ','Kamil')
+age = 25
+print("My age is :", age)
+
+name = 'Kamil'
+age = 25
+height =5.5
+isMarried = False
+# isMarried2 = false
+
+print("Name :", name)
+print("age :", age)
+print("height :", height)
+print("isMarried :", isMarried)
+# print("isMarried2 :", isMarried2)
+
