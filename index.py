ca28337d279575a94dd1a58d4c97030b5a4b7f63
Kamil-shehzad <75947637+Kamil-shehzad@users.noreply.github.com>
2024-10-19 18:41:20 -0700

    Data types in python

--- index.py
+++ index.py
@@ -5,6 +5,7 @@ age = 25
 print("My age is :", age)
 
 name = 'Kamil'
+#  Python is a case sensitive  laguage mean capital and small letters are consider separate variables.
 age = 25
 height =5.5
 isMarried = False
@@ -15,4 +16,19 @@ print("age :", age)
 print("height :", height)
 print("isMarried :", isMarried)
 # print("isMarried2 :", isMarried2)
+
+#Data Types 
+
+integer = 10
+float_number = 5.5
+string = 'Hello World'
+boolean = True
+#"True" or "False" are correct in Python while "false" and "true" are incorrect
+dataTypeNone = None
+
+print(type(integer))
+print(type(float_number))
+print(type(string))
+print(type(boolean))
+print(type(dataTypeNone))
 
